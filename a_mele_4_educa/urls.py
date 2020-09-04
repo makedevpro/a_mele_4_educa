8d3030b030ac81a4694a068f42af7292205fdeb2
makedevpro <makedevpro@gmail.com>
2020-09-04 13:26:21 +0300

    10. CMS. Added auth views by django auth and added html template for it.

--- a_mele_4_educa/urls.py
+++ a_mele_4_educa/urls.py
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.contrib.staticfiles.urls import static
 from django.urls import path
 
 urlpatterns = [
+    path('accounts/login', auth_views.LoginView.as_view(), name='login'),
+    path('accounts/logout', auth_views.LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
