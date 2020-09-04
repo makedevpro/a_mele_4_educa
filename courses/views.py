6b75491b22d96a22ef03f4e2b02befd731be81cb
makedevpro <makedevpro@gmail.com>
2020-09-05 02:28:07 +0300

    10. Install django-braces, added JQuery sortable by order with drag and drop for modules and them content

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,5 @@
+from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
+
 from django.apps import apps
 from django.contrib.auth.mixins import LoginRequiredMixin, \
     PermissionRequiredMixin
@@ -190,3 +192,29 @@ class ModuleContentListView(TemplateResponseMixin, View):
                                    id=module_id,
                                    course__owner=requst.user)
         return self.render_to_response({'module': module})
+
+
+class ModuleOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
+    """
+    Обработчик, который будет получать новый порядок модулей курса
+    в формате JSON
+    """
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Module.objects.filter(id=id,
+                                  course__owner=request.user)\
+                .update(order=order)
+        return self.render_json_response({'saved': 'OK'})
+
+
+class ContentOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
+    """
+    Обработчик, который будет получать новый порядок содержимого модулей курса
+    в формате JSON
+    """
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Content.objects.filter(id=id,
+                                   module__course__owner=request.user)\
+                .update(order=order)
+        return self.render_json_response({'status': 'OK'})
