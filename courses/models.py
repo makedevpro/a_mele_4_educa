7980929cf03060139034aacbeedd10dd65e96508
makedevpro <makedevpro@gmail.com>
2020-09-04 12:54:45 +0300

    10. Added custom OrderingField based on PositiveIntegerField.

--- courses/models.py
+++ courses/models.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 
+from .fields import OrderField
+
 
 class Subject(models.Model):
     title = models.CharField(max_length=200)
@@ -40,9 +42,10 @@ class Module(models.Model):
                                on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
+    order = OrderField(blank=True, for_fields=['course'])
 
     def __str__(self) -> str:
-        return f'{self.title}'
+        return f'{self.order}. {self.title}'
 
 
 class Content(models.Model):
@@ -59,6 +62,11 @@ class Content(models.Model):
                                      )})
     object_id = models.PositiveIntegerField()
     item = GenericForeignKey('content_type', 'object_id')
+    #  порядковые номера объектов будут задаваться в рамках одного модуля
+    order = OrderField(blank=True, for_fields=['module'])
+
+    class Meta:
+        ordering = ['order']
 
 
 class ItemBase(models.Model):
