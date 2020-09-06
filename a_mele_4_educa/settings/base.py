93b1bc68574a7193aae11991f0a921ee72403f20
makedevpro <makedevpro@gmail.com>
2020-09-06 23:22:55 +0300

    13. Added subdomain_course_middleware and chanche settings for work with *.educaproject.com.

--- a_mele_4_educa/settings/base.py
+++ a_mele_4_educa/settings/base.py
@@ -59,6 +59,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'courses.middleware.subdomain_course_middleware',
 ]
 
 ROOT_URLCONF = 'a_mele_4_educa.urls'
