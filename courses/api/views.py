2f6efd68350bb51aed9f5d68e5f93c313a4c69fd
makedevpro <makedevpro@gmail.com>
2020-09-06 16:15:01 +0300

    12. Added APIView for enroll student to course

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,6 +1,10 @@
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from ..models import Subject
+from django.shortcuts import get_object_or_404
+
+from ..models import Subject, Course
 from .serializers import SubjectSerializer
 
 
@@ -12,3 +16,11 @@ class SubjectListView(generics.ListAPIView):
 class SubjectDetailView(generics.RetrieveAPIView):
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
+
+
+class CourseEnrollView(APIView):
+    """ Запись студента на курс """
+    def post(self, request, pk, format=None):
+        course = get_object_or_404(Course, pk=pk)
+        course.students.add(request.user)
+        return Response({'enrolled': True})
