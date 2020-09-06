69caad9c2e4424367762b62a42aecfdc09fa7fda
makedevpro <makedevpro@gmail.com>
2020-09-06 18:37:35 +0300

    13. Has been set settings for work with different environments.

--- a_mele_4_educa/settings/base.py
+++ a_mele_4_educa/settings/base.py
@@ -16,8 +16,8 @@ from pathlib import Path
 from django.urls import reverse_lazy
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
-
+BASE_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
+print(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = 'i(=f(op)q5ezq^&@*euyy3zko@j2mxpcrl_j4yhmhblx!e(!l7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -83,16 +83,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'a_mele_4_educa.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
---
+++ a_mele_4_educa/settings/pro.py
@@ -0,0 +1,13 @@
+from .base import *
+
+DEBUG = False
+
+ADMINS = (
+    ('Admin', 'admin@localhost'),
+)
+
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {}
+}
