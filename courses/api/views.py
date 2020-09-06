28b4df24293845f2c17988b59a527fb8a50b7271
makedevpro <makedevpro@gmail.com>
2020-09-06 17:43:41 +0300

    12. Adder routers, ViewSet for courses.

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -7,7 +7,7 @@ from rest_framework.views import APIView
 from django.shortcuts import get_object_or_404
 
 from ..models import Subject, Course
-from .serializers import SubjectSerializer
+from .serializers import SubjectSerializer, CourseSerializer
 
 
 class SubjectListView(generics.ListAPIView):
@@ -32,3 +32,9 @@ class CourseEnrollView(APIView):
         course = get_object_or_404(Course, pk=pk)
         course.students.add(request.user)
         return Response({'enrolled': True})
+
+
+class CourseViewSet(viewsets.ReadOnlyModelViewSet):
+    """ Маршрутизатор с методами retrieve и list """
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
